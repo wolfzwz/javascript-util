365c12a77f2904e14fb34a665584c9a394947fa4
The tiger sniffed the rose <1977099978@qq.com>
2019-05-30 10:13:51 +0800

    Update util.js

--- src/util/util.js
+++ src/util/util.js
@@ -88,5 +88,17 @@ export const util = {
     url += encodeURIComponent(name) + '=' +encodeURIComponent(value);
     return url;
   },
-	
+  // 判断浏览器
+  getBrowserType(){
+  	var Sys = {};
+        var ua = navigator.userAgent.toLowerCase();
+        var s;
+	// 如果是该版本浏览器则Sys对应属性if判断成立，否则没有指，为undefined
+        (s = ua.match(/msie ([\d.]+)/)) ? Sys.ie = s[1] :
+        (s = ua.match(/firefox\/([\d.]+)/)) ? Sys.firefox = s[1] :
+        (s = ua.match(/chrome\/([\d.]+)/)) ? Sys.chrome = s[1] :
+        (s = ua.match(/opera.([\d.]+)/)) ? Sys.opera = s[1] :
+        (s = ua.match(/version\/([\d.]+).*safari/)) ? Sys.safari = s[1] : 0;
+	return Sys;
+  }
 };
